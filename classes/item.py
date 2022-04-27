0d8d1960e6295550235077d9fa071d5552965f1d
orionkendrick <101196816+orionkendrick@users.noreply.github.com>
2022-04-27 17:12:35 -0500

    Updated functions for Items

--- classes/item.py
+++ classes/item.py
@@ -10,11 +10,17 @@ class Item(ApplicationClass):
         self.price = None
         self.quantity = None
 
+
+
     #Item management functions
     def create(self, itemID, itemName, price, quantity):
         try:
-            self.Table.cursor.execute('INSERT INTO items (itemID, itemName, price, quantity) VALUES (?, ?, ?, ?)',(itemID, itemName, price, quantity))
+            self.Table.cursor.execute('INSERT INTO items (itemID, itemName, price, quantity) VALUES (?, ?, ?, ?)',[itemID, itemName, price, quantity])
             self.Table.connection.commit()
+            self.itemID = itemID
+            self.itemName = itemName
+            self.price = price
+            self.quantity = quantity
         except Exception: # Catch any exceptions and return False to signal failure
             return False, 'Item already exists!'
         return True, 'Item created.'
@@ -22,16 +28,22 @@ class Item(ApplicationClass):
     def decrement(self, itemID, num):
         try:
             decrementedQuantity = self.quantity - num
-            self.Table.cursor.execute('UPDATE items SET quantity = ? WHERE itemID = ?'),(decrementedQuantity, itemID)
+            self.Table.cursor.execute('UPDATE items SET quantity = ? WHERE itemID = ?',(decrementedQuantity, itemID))
         except:
             return False, 'Error decrementing item quantity'
     
     def getName(self, itemID):
-        return self.itemName
+        name = self.Table.cursor.execute('SELECT itemName FROM items WHERE itemID=?',[itemID])
+        name = self.Table.cursor.fetchall()
+        return name[0]
     
     def getPrice(self, itemID):
-        return self.price
+        price = self.Table.cursor.execute('SELECT price FROM items WHERE itemID=?',[itemID])
+        price = self.Table.cursor.fetchall()
+        return price[0]
     
     def getQuantity(self, itemID):
-        return self.quantity
+        quantity = self.Table.cursor.execute('SELECT quantity FROM items WHERE itemID=?',[itemID])
+        quantity = self.Table.cursor.fetchall()
+        return quantity[0]
     

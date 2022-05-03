2e4c7b3e15e553f6e5e3be09aa9b2c21998551cf
fr0sted <32829922+fr0sted@users.noreply.github.com>
2022-05-03 13:40:21 -0500

    Update order.py

--- classes/order.py
+++ classes/order.py
@@ -2,7 +2,11 @@ from lib.applicationclass import ApplicationClass
 
 # In this example, I'll create a User class that inherits from ApplicationClass
 class Order(ApplicationClass):
-    def __init__(self):
-        pass
+    def __init__(self, user):
+        ApplicationClass.__init__(self)
+        
+        self.user = user
+        
+        self.__updateOrders()
     
    

5442d1e07ce6dfa889430af206ce40afc0773aa6
fr0sted <32829922+fr0sted@users.noreply.github.com>
2022-05-03 23:06:10 -0500

    Update order.py

--- classes/order.py
+++ classes/order.py
@@ -12,4 +12,25 @@ class Order(ApplicationClass):
 
         self.cart = None
     
-   
+   from lib.applicationclass import ApplicationClass
+
+# In this example, I'll create a Order class that inherits from ApplicationClass
+class Order(ApplicationClass):
+    def __init__(self):
+      
+        self.orderID = 0
+        self.items[]
+
+    
+#initializer method    
+    def Order(self, orderID):
+    
+#lists all orders associated with the user
+    def viewOrders(self, items):
+      for item in orderID.items:
+        print(f'Items are: {orderID.items}')
+
+#cancels the oder
+    def cancel(self):
+      orderToCancel= input('Enter orderID of order you would like to cancel:')
+      return void f"You chose to cancel order: {orderToCancel}"
